5ef8460b36fab7bd4775931e8457f74a98dbb1f4
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2021-12-01 14:06:50 +0100

    AdventOfCode Day 1 Part 2

--- src/AdventOfCode/Day1.cs
+++ src/AdventOfCode/Day1.cs
@@ -5,13 +5,23 @@ public class Day1
     {
         var increases = 0;
         var previousDepth = int.MaxValue;
-        var depths = File.ReadAllLines("AdventOfCode\\Data\\Day1Input.txt");
-        foreach (var depthStr in depths)
+        var depths = File.ReadAllLines("AdventOfCode\\Data\\Day1Input.txt").Select(d => int.Parse(d)).ToList();
+        var numberOfDepths = depths.Count();
+        foreach (var depth in depths)
         {
-            var depth = int.Parse(depthStr);
             if (depth > previousDepth) increases++;
             previousDepth = depth;
         }
-        Console.WriteLine($"AdventOfCode Day 1 result: {increases}");
+        Console.WriteLine($"AdventOfCode Day 1 Part 1 result: {increases}");
+
+        increases = 0;
+        previousDepth = int.MaxValue;
+        for (int index = 0; index + 2 < numberOfDepths; index++)
+        {
+            var depth = depths[index] + depths[index + 1] + depths[index + 2];
+            if (depth > previousDepth) increases++;
+            previousDepth = depth;
+        }
+        Console.WriteLine($"AdventOfCode Day 1 Part 2 result: {increases}");
     }
 }
