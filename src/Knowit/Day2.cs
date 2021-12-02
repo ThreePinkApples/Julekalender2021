202714036e51b19e56690913ea9dbf5a5a366787
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-02 18:11:48 +0100

    Knowit Day 2 - Try to map to 3D vector. Failed

--- src/Knowit/Day2.cs
+++ src/Knowit/Day2.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 namespace AdventCalendar2021.Knowit;
 public class Day2
 {
+    private const int RadiusEarth = 6371;
 
     public static void Run()
     {
@@ -11,21 +12,20 @@ public class Day2
         var cities = File.ReadAllLines("Knowit\\Data\\cities.csv")
             .Select(line => LineToCity(line, regex))
             .ToList();
-        var radius = 6371;
         // Assuming the north pole vector ¯\_(ツ)_/¯
-        var start = new Vector2(90, 90);
+        var start = CoordinatesToVector3(90, 90);
         var currentPosition = start;
         var totalDistance = 0f;
         while (cities.Count > 0)
         {
-            var nextCity = cities.OrderBy(c => Vector2.Distance(currentPosition, c.Position)).First();
+            var nextCity = cities.OrderBy(c => Vector3.Distance(currentPosition, c.Position)).First();
             cities.Remove(nextCity);
-            var distance = Vector2.Distance(currentPosition, nextCity.Position);
-            totalDistance += (distance / 180) * radius;
+            var distance = Vector3.Distance(currentPosition, nextCity.Position);
+            totalDistance += distance;
             currentPosition = nextCity.Position;
         }
         // Back to the north pole
-        totalDistance += (Vector2.Distance(currentPosition, start) / 180) * radius;
+        totalDistance += Vector3.Distance(currentPosition, start);
         currentPosition = start;
         Console.WriteLine($"Total distance {totalDistance}");
     }
@@ -35,20 +35,34 @@ public class Day2
         var regexMatch = regex.Match(line);
         return new City(
             regexMatch.Groups[1].Value,
-            float.Parse(regexMatch.Groups[2].Value.Replace(".", ",")),
-            float.Parse(regexMatch.Groups[3].Value.Replace(".", ","))
+            CoordinatesToVector3(
+                float.Parse(regexMatch.Groups[2].Value.Replace(".", ",")),
+                float.Parse(regexMatch.Groups[3].Value.Replace(".", ","))
+            )
         );
+    }
+
+    public static Vector3 CoordinatesToVector3(float latitude, float longitude)
+    {
+        double latitude_rad = latitude * Math.PI / 180;
+        double longitude_rad = longitude * Math.PI / 180;
+
+        double zPos = RadiusEarth * Math.Cos(latitude_rad) * Math.Cos(longitude_rad);
+        double xPos = -RadiusEarth * Math.Cos(latitude_rad) * Math.Sin(longitude_rad);
+        double yPos = RadiusEarth * Math.Sin(latitude_rad);
+
+        return new Vector3((float)xPos, (float)yPos, (float)zPos);
     }
 
     internal class City
     {
         public string Name { get; set; }
-        public Vector2 Position { get; set; }
+        public Vector3 Position { get; set; }
 
-        public City(string name, float x, float y)
+        public City(string name, Vector3 position)
         {
             Name = name;
-            Position = new Vector2(x, y);
+            Position = position;
         }
     }
 }
