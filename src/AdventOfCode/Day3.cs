2c07598ce2e60c4198b75c8d39e915a7431eba5b
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2021-12-03 13:48:32 +0100

    AdventOfCode Day 3 clean imports

--- src/AdventOfCode/Day3.cs
+++ src/AdventOfCode/Day3.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventCalendar2021.AdventOfCode;
+﻿namespace AdventCalendar2021.AdventOfCode;
 public class Day3
 {
     public static void Run()
