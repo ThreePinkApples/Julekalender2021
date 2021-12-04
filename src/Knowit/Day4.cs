b7eb3b7fc57c87ef0af918b5a32668b37b5a8ec1
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-04 16:58:15 +0100

    Knowit Day 4 brute-force solution
    This will run "forever", but I have no
    idea how to solve this properly

---
+++ src/Knowit/Day4.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace AdventCalendar2021.Knowit;
+public class Day4
+{
+    public static void Run()
+    {
+        var X = BigInteger.Zero;
+        var Y = BigInteger.Zero;
+        var moves = BigInteger.Parse("100000000000000000079");
+        var up = true;
+        while (true)
+        {
+            if (up)
+            {
+                Y++;
+                if (Y % 15 != 0)
+                    up = !up;
+            }
+            else
+            {
+                X++;
+                if (X % 15 != 0)
+                    up = !up;
+            }
+
+            if (X + Y == moves)
+                break;
+        }
+        Console.WriteLine($"Position {X},{Y}");
+    }
+}
