2a2d59127f9711d1d6fd4533c778a73ebec2f56f
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-05 12:05:55 +0100

    AdventOfCode Day 5 Part 2 - Solved!

--- src/AdventOfCode/Day5.cs
+++ src/AdventOfCode/Day5.cs
@@ -4,6 +4,12 @@ public class Day5
     public static void Run()
     {
         var input = File.ReadAllLines("AdventOfCode\\Data\\Day5Input.txt");
+        Part1(input);
+        Part2(input);
+    }
+
+    public static void Part1(string[] input)
+    {
         var matrix = new int[1000, 1000];
         foreach (var line in input)
         {
@@ -27,6 +33,40 @@ public class Day5
         }
         var result = matrix.Cast<int>().Count(n => n > 1);
         Console.WriteLine($"AdventOfCode Day 5 Part 1 {result}");
+    }
+
+    public static void Part2(string[] input)
+    {
+        var matrix = new int[1000, 1000];
+        foreach (var line in input)
+        {
+            var parts = line.Split(" -> ").SelectMany(p => p.Split(",")).Select(int.Parse).ToList();
+            var start = new IntVector2(parts[0], parts[1]);
+            var end = new IntVector2(parts[2], parts[3]);
+            var XDirection = end.X > start.X ? 1 : end.X < start.X ? -1 : 0;
+            var YDirection = end.Y > start.Y ? 1 : end.Y < start.Y ? -1 : 0;
+            if (XDirection != 0 && YDirection != 0)
+            {
+                // Line is diagonal, but we should only accept perfect 45 degrees
+                var XDiff = Math.Abs(start.X - end.X);
+                var YDiff = Math.Abs(start.Y - end.Y);
+                if (XDiff != YDiff) continue;
+                for (int diff = 0; diff <= XDiff; diff++)
+                    matrix[start.X + (diff * XDirection), start.Y + (diff * YDirection)]++;
+            }
+            else if (XDirection != 0)
+            {
+                for (int X = start.X; X != end.X + XDirection; X += XDirection)
+                    matrix[X, start.Y]++;
+            }
+            else if (YDirection != 0)
+            {
+                for (int Y = start.Y; Y != end.Y + YDirection; Y += YDirection)
+                    matrix[start.X, Y]++;
+            }
+        }
+        var result = matrix.Cast<int>().Count(n => n > 1);
+        Console.WriteLine($"AdventOfCode Day 5 Part 2 {result}");
     }
 
     internal struct IntVector2
