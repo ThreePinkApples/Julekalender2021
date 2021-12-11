906ec8a4dbe15a8f7d367c9e2eb99def3396aa2a
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-11 23:05:35 +0100

    AdventOfCode Day 10 Part 2 - Solved!

--- src/AdventOfCode/Day10.cs
+++ src/AdventOfCode/Day10.cs
@@ -1,44 +1,57 @@
 ﻿namespace AdventCalendar2021.AdventOfCode;
 public class Day10
 {
-    private static char[] OpenTokens = new char[] { '[', '{', '<', '(' };
-    private static Dictionary<char, char> CloseToOpenTokenMap = new()
+    private static Dictionary<char, char> OpenToCloseTokenMap = new()
     {
-        { ']', '[' },
-        { '}', '{' },
-        { '>', '<' },
-        { ')', '(' }
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+        { '<', '>' }
     };
 
-    private static Dictionary<char, int> PointMap = new()
+    private static Dictionary<char, char> CloseToOpenTokenMap = new()
     {
+        { ')', '(' },
+        { ']', '[' },
+        { '}', '{' },
+        { '>', '<' }
+    };
+
+    private static Dictionary<char, int> SyntaxErrorPointMap = new()
+    {
+        { ')', 3 },
         { ']', 57 },
         { '}', 1197 },
         { '>', 25137 },
-        { ')', 3 }
+    };
+
+    private static Dictionary<char, int> IncompletePointMap = new()
+    {
+        { ')', 1 },
+        { ']', 2 },
+        { '}', 3 },
+        { '>', 4 }
     };
 
     public static void Run()
     {
         var input = File.ReadAllLines("AdventOfCode\\Data\\Day10Input.txt").ToList();
-        Part1(input);
-    }
-
-    public static void Part1(List<string> input)
-    {
-        var totalPoints = 0;
+        var syntaxErrorScore = 0;
+        var incompleteScores = new List<double>();
         foreach (var line in input)
         {
             var tokenStack = new Stack<char>();
+            var syntaxError = false;
             foreach (var token in line)
             {
-                if (OpenTokens.Contains(token))
+                if (OpenToCloseTokenMap.Keys.Contains(token))
                 {
                     tokenStack.Push(token);
                 }
                 else if (tokenStack.Peek() != CloseToOpenTokenMap[token])
                 {
-                    totalPoints += PointMap[token];
+                    syntaxErrorScore += SyntaxErrorPointMap[token];
+                    syntaxError = true;
                     break;
                 }
                 else
@@ -46,7 +59,20 @@ public class Day10
                     tokenStack.Pop();
                 }
             }
+            if (!syntaxError && tokenStack.Count > 0)
+            {
+                // Line is incomplete
+                var lineScore = 0.0;
+                foreach (var remainingToken in tokenStack)
+                {
+                    lineScore *= 5;
+                    lineScore += IncompletePointMap[OpenToCloseTokenMap[remainingToken]];
+                }
+                incompleteScores.Add(lineScore);
+            }
         }
-        Console.Write($"AdventOfCode Day 10 Part 1: {totalPoints}");
+        Console.WriteLine($"AdventOfCode Day 10 Part 1: {syntaxErrorScore}");
+        var incompleteFinalScore = incompleteScores.OrderBy(s => s).ToList()[incompleteScores.Count / 2];
+        Console.WriteLine($"AdventOfCode Day 10 Part 2: {incompleteFinalScore}");
     }
 }
