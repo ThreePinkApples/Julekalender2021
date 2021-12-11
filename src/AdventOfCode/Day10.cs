e09850af92ff4582ed21908eafd9603ea4004831
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-11 22:52:25 +0100

    AdventOfCode Day 10 Part 1 - Solved!

---
+++ src/AdventOfCode/Day10.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventCalendar2021.AdventOfCode;
+public class Day10
+{
+    private static char[] OpenTokens = new char[] { '[', '{', '<', '(' };
+    private static Dictionary<char, char> CloseToOpenTokenMap = new()
+    {
+        { ']', '[' },
+        { '}', '{' },
+        { '>', '<' },
+        { ')', '(' }
+    };
+
+    private static Dictionary<char, int> PointMap = new()
+    {
+        { ']', 57 },
+        { '}', 1197 },
+        { '>', 25137 },
+        { ')', 3 }
+    };
+
+    public static void Run()
+    {
+        var input = File.ReadAllLines("AdventOfCode\\Data\\Day10Input.txt").ToList();
+        Part1(input);
+    }
+
+    public static void Part1(List<string> input)
+    {
+        var totalPoints = 0;
+        foreach (var line in input)
+        {
+            var tokenStack = new Stack<char>();
+            foreach (var token in line)
+            {
+                if (OpenTokens.Contains(token))
+                {
+                    tokenStack.Push(token);
+                }
+                else if (tokenStack.Peek() != CloseToOpenTokenMap[token])
+                {
+                    totalPoints += PointMap[token];
+                    break;
+                }
+                else
+                {
+                    tokenStack.Pop();
+                }
+            }
+        }
+        Console.Write($"AdventOfCode Day 10 Part 1: {totalPoints}");
+    }
+}
