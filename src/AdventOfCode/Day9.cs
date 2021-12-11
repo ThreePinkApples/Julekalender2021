226cd39b3793dd5cc3e8cacf6903891fe3ff3f9a
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-11 21:37:03 +0100

    AdventOfCode Day 9 Part 1 - Solved!

--- src/AdventOfCode/Day9.cs
+++ src/AdventOfCode/Day9.cs
@@ -4,17 +4,33 @@ public class Day9
 
     public static void Run()
     {
-        var input = File.ReadAllLines("AdventOfCode\\Data\\Day9Input.txt").Select(l => l.Select(c => (int)char.GetNumericValue(c)));
+        var input = File.ReadAllLines("AdventOfCode\\Data\\Day9Input.txt").Select(l => l.Select(c => (int)char.GetNumericValue(c)).ToList()).ToList();
         Part1(input);
         Part2(input);
     }
 
-    private static void Part1(IEnumerable<IEnumerable<int>> HeightMap)
+    private static void Part1(List<List<int>> HeightMap)
     {
-        Console.WriteLine($"AdventOfCode Day 9 Part 1 result: ");
+        var totalLowPointRiskLevel = 0;
+        for (int rowIndex = 0; rowIndex < HeightMap.Count; rowIndex++)
+        {
+            for (int columnIndex = 0; columnIndex < HeightMap[rowIndex].Count; columnIndex++)
+            {
+                var currentHeight = HeightMap[rowIndex][columnIndex];
+                var up = rowIndex != 0 ? HeightMap[rowIndex - 1][columnIndex] : 10;
+                var down = rowIndex != HeightMap.Count - 1 ? HeightMap[rowIndex + 1][columnIndex] : 10;
+                var left = columnIndex != 0 ? HeightMap[rowIndex][columnIndex - 1] : 10;
+                var right = columnIndex != HeightMap[rowIndex].Count - 1 ? HeightMap[rowIndex][columnIndex + 1] : 10;
+                if (currentHeight < up && currentHeight < down && currentHeight < left && currentHeight < right)
+                {
+                    totalLowPointRiskLevel += 1 + currentHeight;
+                }
+            }
+        }
+        Console.WriteLine($"AdventOfCode Day 9 Part 1 result: {totalLowPointRiskLevel}");
     }
 
-    private static void Part2(IEnumerable<IEnumerable<int>> HeightMap)
+    private static void Part2(List<List<int>> HeightMap)
     {
         Console.WriteLine($"AdventOfCode Day 9 Part 2 result: ");
     }
