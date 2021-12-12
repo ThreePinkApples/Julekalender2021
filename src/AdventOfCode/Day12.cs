e193eb65ca56960ee8313a4d092c5d26c06b3363
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2021-12-12 15:39:23 +0100

    AdventOfCode Day 12 Part 2 - Solved!

--- src/AdventOfCode/Day12.cs
+++ src/AdventOfCode/Day12.cs
@@ -4,28 +4,6 @@ public class Day12
     public static void Run()
     {
         var input = File.ReadAllLines("AdventOfCode\\Data\\Day12Input.txt").ToList();
-        var exampleInput = new List<string>()
-        {
-            "dc-end",
-            "HN-start",
-            "start-kj",
-            "dc-start",
-            "dc-HN",
-            "LN-dc",
-            "HN-end",
-            "kj-sa",
-            "kj-HN",
-            "kj-dc"
-        };
-        var exampleCaves = exampleInput.SelectMany(i => i.Split("-")).ToHashSet().Select(c => new Cave(c)).ToHashSet();
-        var exampleCavesMapped = exampleCaves.ToDictionary(c => c.Name);
-        exampleInput.ForEach(connection =>
-        {
-            var parts = connection.Split("-");
-            exampleCavesMapped[parts[0]].AddConnection(exampleCavesMapped[parts[1]]);
-            exampleCavesMapped[parts[1]].AddConnection(exampleCavesMapped[parts[0]]);
-        });
-        Part1(exampleCavesMapped);
         var caves = input.SelectMany(i => i.Split("-")).ToHashSet().Select(c => new Cave(c)).ToHashSet();
         var cavesMapped = caves.ToDictionary(c => c.Name);
         input.ForEach(connection =>
@@ -35,35 +13,56 @@ public class Day12
             cavesMapped[parts[1]].AddConnection(cavesMapped[parts[0]]);
         });
         Part1(cavesMapped);
+        Part2(cavesMapped);
     }
 
     private static void Part1(Dictionary<string, Cave> cavesMapped)
     {
         var start = cavesMapped["start"];
         var end = cavesMapped["end"];
-        var paths = FindPaths(start, end, new() { start });
+        var paths = FindPaths(start, end, new() { start }, false);
         Console.WriteLine($"AdventOfCode Day 12 Part 1: {paths.Count}");
     }
 
-    private static HashSet<List<Cave>> FindPaths(Cave start, Cave end, List<Cave> visitedSmallCaves)
+    private static void Part2(Dictionary<string, Cave> cavesMapped)
+    {
+        var start = cavesMapped["start"];
+        var end = cavesMapped["end"];
+        var paths = FindPaths(start, end, new() { start }, true);
+        // Didn't bother to find out how to eliminate duplicates in the FindPaths
+        // method, so just using the magic of HashSet on strings to take care of it
+        var uniquePaths = paths.Select(path => string.Join(',', path)).ToHashSet();
+        Console.WriteLine($"AdventOfCode Day 12 Part 2: {uniquePaths.Count}");
+    }
+
+    private static HashSet<List<Cave>> FindPaths(Cave start, Cave end, HashSet<Cave> visitedSmallCaves, bool allowSmallDouble = false)
     {
         var paths = new HashSet<List<Cave>>();
-        foreach (var connectedCave in start.ConnectedCaves)
+        var runs = 1;
+        if (allowSmallDouble && start.Name != "start" && start.Size == CaveSize.SMALL)
+            // Run through subpaths twice. Once were we do not allow revisiting 'start',
+            // and once were revisting is allowed.
+            runs = 2;
+        for (int run = 1; run <= runs; run++)
         {
-            var path = new List<Cave>() { start };
-            var subCavesVisited = visitedSmallCaves.ToList();
-            if (connectedCave == end)
+            if (run == 2) visitedSmallCaves.Remove(start);
+            foreach (var connectedCave in start.ConnectedCaves)
             {
-                path.Add(end);
-                paths.Add(path);
-                continue;
+                var path = new List<Cave>() { start };
+                var subCavesVisited = visitedSmallCaves.ToHashSet();
+                if (connectedCave == end)
+                {
+                    path.Add(end);
+                    paths.Add(path);
+                    continue;
+                }
+                else if (visitedSmallCaves.Contains(connectedCave))
+                    continue;
+                if (connectedCave.Size == CaveSize.SMALL)
+                    subCavesVisited.Add(connectedCave);
+                var subPaths = FindPaths(connectedCave, end, subCavesVisited, allowSmallDouble && run == 1);
+                subPaths.ToList().ForEach(subPath => paths.Add(path.Concat(subPath).ToList()));
             }
-            else if (visitedSmallCaves.Contains(connectedCave))
-                continue;
-            if (connectedCave.Size == CaveSize.SMALL)
-                subCavesVisited.Add(connectedCave);
-            var subPaths = FindPaths(connectedCave, end, subCavesVisited);
-            subPaths.ToList().ForEach(subPath => paths.Add(path.Concat(subPath).ToList()));
         }
         return paths;
     }
