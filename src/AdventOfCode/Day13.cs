6a522a0d3a2109ee41e0255cb41504f0acf2f24f
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2021-12-13 08:40:22 +0100

    AdventOfCode Day 13 prep

---
+++ src/AdventOfCode/Day13.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventCalendar2021.AdventOfCode;
+public class Day13
+{
+    public static void Run()
+    {
+        var input = File.ReadAllLines("AdventOfCode\\Data\\Day13Input.txt");
+    }
+
+    private static void Part1()
+    {
+        Console.WriteLine($"AdventOfCode Day 13 Part 1: ");
+    }
+
+    private static void Part2()
+    {
+        Console.WriteLine($"AdventOfCode Day 13 Part 2: ");
+    }
+}
