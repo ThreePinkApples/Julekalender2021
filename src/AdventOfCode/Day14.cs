934d26f12ca3cd79f375b8e26a9fe13618e7fbf6
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2021-12-14 12:51:18 +0100

    AdventOfCode Day 14 Part 2 - Too slow

--- src/AdventOfCode/Day14.cs
+++ src/AdventOfCode/Day14.cs
@@ -8,28 +8,38 @@ public class Day14
         var input = File.ReadAllLines("AdventOfCode\\Data\\Day14Input.txt").ToList();
         var polymerTemplate = input[0];
         var pairInsertions = input.GetRange(2, input.Count - 2).Select(PairInsertion.FromLine);
-        Part1(polymerTemplate, pairInsertions.ToList());
+        InsertPolymers(polymerTemplate, pairInsertions.ToList());
     }
 
-    private static void Part1(string polymerTemplate, List<PairInsertion> pairInsertions)
+    private static void InsertPolymers(string polymerTemplate, List<PairInsertion> pairInsertions)
     {
-        for (int step = 1; step <= 10; step++)
+        for (int step = 1; step <= 40; step++)
         {
             var newTemplate = polymerTemplate;
             for (int index = 0; index < polymerTemplate.Length - 1; index++)
             {
                 var newInsert = pairInsertions.Where(p => p.Pair == $"{polymerTemplate[index]}{polymerTemplate[index + 1]}").Single();
-                var numberOfInserts = newTemplate.Length - polymerTemplate.Length;
-                newTemplate = newTemplate.Substring(0, index + numberOfInserts + 1) + newInsert.Insert + newTemplate.Substring(index + numberOfInserts + 1);
+                newTemplate = newTemplate.Insert(index + 1, newInsert.Insert.ToString());
             }
             polymerTemplate = newTemplate;
+            if (step == 10)
+            {
+                var letterFrequenciesPart1 = GetLetterFrequencies(polymerTemplate);
+                var resultPart1 = letterFrequenciesPart1.First().Item2 - letterFrequenciesPart1.Last().Item2;
+                Console.WriteLine($"AdventOfCode Day 14 Part 1: {resultPart1}");
+            }
         }
-        var letterFrequencies = polymerTemplate
+        var letterFrequencies = GetLetterFrequencies(polymerTemplate);
+        var result = letterFrequencies.First().Item2 - letterFrequencies.Last().Item2;
+        Console.WriteLine($"AdventOfCode Day 14 Part 2: {result}");
+    }
+
+    private static IEnumerable<Tuple<char, int>> GetLetterFrequencies(string text)
+    {
+        return text
             .GroupBy(c => c)
-            .Select(g => new { Letter = g.Key, Count = g.Count()})
-            .OrderByDescending(n => n.Count);
-        var result = letterFrequencies.First().Count - letterFrequencies.Last().Count;
-        Console.WriteLine($"AdventOfCode Day 14 Part 1: {result}");
+            .Select(g => new Tuple<char, int>(g.Key, g.Count()))
+            .OrderByDescending(t => t.Item2);
     }
 
     internal struct PairInsertion
