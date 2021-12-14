15bee626becb5597a281f88aa3f55f8183e713fe
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2021-12-14 09:56:01 +0100

    AdventOfCode Day 14 Part 1 - Solved!

--- src/AdventOfCode/Day14.cs
+++ src/AdventOfCode/Day14.cs
@@ -13,7 +13,23 @@ public class Day14
 
     private static void Part1(string polymerTemplate, List<PairInsertion> pairInsertions)
     {
-
+        for (int step = 1; step <= 10; step++)
+        {
+            var newTemplate = polymerTemplate;
+            for (int index = 0; index < polymerTemplate.Length - 1; index++)
+            {
+                var newInsert = pairInsertions.Where(p => p.Pair == $"{polymerTemplate[index]}{polymerTemplate[index + 1]}").Single();
+                var numberOfInserts = newTemplate.Length - polymerTemplate.Length;
+                newTemplate = newTemplate.Substring(0, index + numberOfInserts + 1) + newInsert.Insert + newTemplate.Substring(index + numberOfInserts + 1);
+            }
+            polymerTemplate = newTemplate;
+        }
+        var letterFrequencies = polymerTemplate
+            .GroupBy(c => c)
+            .Select(g => new { Letter = g.Key, Count = g.Count()})
+            .OrderByDescending(n => n.Count);
+        var result = letterFrequencies.First().Count - letterFrequencies.Last().Count;
+        Console.WriteLine($"AdventOfCode Day 14 Part 1: {result}");
     }
 
     internal struct PairInsertion
